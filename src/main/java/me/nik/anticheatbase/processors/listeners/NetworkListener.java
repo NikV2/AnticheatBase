977267a0b7fcc2d438fdc418ac67999564dbf429
Nik <61439310+NikV2@users.noreply.github.com>
2021-11-09 12:46:00 +0200

    Better

--- src/main/java/me/nik/anticheatbase/processors/listeners/NetworkListener.java
+++ src/main/java/me/nik/anticheatbase/processors/listeners/NetworkListener.java
@@ -5,13 +5,16 @@ import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
-import com.google.common.collect.ImmutableSet;
 import me.nik.anticheatbase.Anticheat;
 import me.nik.anticheatbase.playerdata.Profile;
 import me.nik.anticheatbase.processors.Packet;
-import me.nik.anticheatbase.utils.ServerUtils;
 import me.nik.anticheatbase.wrappers.WrapperPlayServerEntityVelocity;
 import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A network listener class that we'll use in order to listen for received and sent packets for our checks and data.
@@ -25,44 +28,54 @@ public class NetworkListener extends PacketAdapter {
     This ensures that we'll be maximizing our perfomance by not processing any
     Packets that we're never using.
      */
-    private static final ImmutableSet<PacketType> WHITELISTED_PACKETS = new ImmutableSet.Builder<PacketType>()
-            .add(
-                    /*
-                    Clientbound Packets
-                     */
-                    ServerUtils.isCavesUpdate() ? PacketType.Play.Client.GROUND : PacketType.Play.Client.FLYING,
-                    PacketType.Play.Client.POSITION,
-                    PacketType.Play.Client.POSITION_LOOK,
-                    PacketType.Play.Client.LOOK,
-                    PacketType.Play.Client.ABILITIES,
-                    PacketType.Play.Client.ARM_ANIMATION,
-                    PacketType.Play.Client.BLOCK_DIG,
-                    PacketType.Play.Client.BLOCK_PLACE,
-                    PacketType.Play.Client.CHAT,
-                    PacketType.Play.Client.ENTITY_ACTION,
-                    PacketType.Play.Client.KEEP_ALIVE,
-                    ServerUtils.isCavesUpdate() ? PacketType.Play.Client.PONG : PacketType.Play.Client.TRANSACTION,
-                    PacketType.Play.Client.RESOURCE_PACK_STATUS,
-                    PacketType.Play.Client.SETTINGS,
-                    PacketType.Play.Client.SPECTATE,
-                    PacketType.Play.Client.STEER_VEHICLE,
-                    PacketType.Play.Client.USE_ENTITY,
-                    PacketType.Play.Client.USE_ITEM,
-                    PacketType.Play.Client.VEHICLE_MOVE,
-                    PacketType.Play.Client.WINDOW_CLICK,
-                    PacketType.Play.Client.SET_CREATIVE_SLOT,
-                    PacketType.Play.Client.HELD_ITEM_SLOT
-            ).add(
-                    /*
-                    Serverbound Packets
-                     */
-                    PacketType.Play.Server.EXPLOSION,
-                    PacketType.Play.Server.ENTITY_VELOCITY,
-                    PacketType.Play.Server.KEEP_ALIVE,
-                    PacketType.Play.Server.ABILITIES,
-                    PacketType.Play.Server.POSITION,
-                    ServerUtils.isCavesUpdate() ? PacketType.Play.Server.PING : PacketType.Play.Server.TRANSACTION
-            ).build();
+    private static final List<PacketType> WHITELISTED_PACKETS = new CopyOnWriteArrayList<>(Stream.of(
+            /*
+            Clientbound Packets
+             */
+            PacketType.Play.Client.FLYING,
+            PacketType.Play.Client.GROUND,
+            PacketType.Play.Client.POSITION,
+            PacketType.Play.Client.POSITION_LOOK,
+            PacketType.Play.Client.LOOK,
+            PacketType.Play.Client.ABILITIES,
+            PacketType.Play.Client.ARM_ANIMATION,
+            PacketType.Play.Client.BLOCK_DIG,
+            PacketType.Play.Client.BLOCK_PLACE,
+            PacketType.Play.Client.CHAT,
+            PacketType.Play.Client.ENTITY_ACTION,
+            PacketType.Play.Client.KEEP_ALIVE,
+            PacketType.Play.Client.PONG,
+            PacketType.Play.Client.TRANSACTION,
+            PacketType.Play.Client.RESOURCE_PACK_STATUS,
+            PacketType.Play.Client.SETTINGS,
+            PacketType.Play.Client.SPECTATE,
+            PacketType.Play.Client.STEER_VEHICLE,
+            PacketType.Play.Client.USE_ENTITY,
+            PacketType.Play.Client.USE_ITEM,
+            PacketType.Play.Client.VEHICLE_MOVE,
+            PacketType.Play.Client.WINDOW_CLICK,
+            PacketType.Play.Client.SET_CREATIVE_SLOT,
+            PacketType.Play.Client.HELD_ITEM_SLOT,
+            /*
+            Serverbound Packets
+             */
+            PacketType.Play.Server.EXPLOSION,
+            PacketType.Play.Server.ENTITY_VELOCITY,
+            PacketType.Play.Server.KEEP_ALIVE,
+            PacketType.Play.Server.ABILITIES,
+            PacketType.Play.Server.POSITION,
+            PacketType.Play.Server.PING,
+            PacketType.Play.Server.TRANSACTION
+
+            /*
+            Remove any packets that are not supported
+            In the current server version.
+
+            ProtocolLib removes them automatically
+            However with this we're ensuring
+            There won't be any warnings.
+             */
+    ).filter(PacketType::isSupported).collect(Collectors.toList()));
 
     public NetworkListener(Anticheat plugin) {
         super(plugin, ListenerPriority.MONITOR, WHITELISTED_PACKETS);
